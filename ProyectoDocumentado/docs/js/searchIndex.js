25ce930d28d3c808af2e9297504f32c126055636
AndresAbonjo <168089383+AndresAbonjo@users.noreply.github.com>
2024-04-26 18:16:32 +0200

    Add files via upload

---
+++ ProyectoDocumentado/docs/js/searchIndex.js
@@ -0,0 +1,54 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Conexion",
+            "name": "Conexion",
+            "summary": "Clase\u0020para\u0020establecer\u0020la\u0020conexi\u00F3n\u0020con\u0020la\u0020BD",
+            "url": "classes/Conexion.html"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructoir\u0020que\u0020establece\u0020la\u0020conexi\u00F3n\u0020con\u0020la\u0020BD\u0020mvc1,\u0020con\u0020usuario\u0020root\u0020en\u0020un\u0020servidor\u0020local",
+            "url": "classes/Conexion.html#method___construct"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024con",
+            "name": "con",
+            "summary": "",
+            "url": "classes/Conexion.html#property_con"
+        },                {
+            "fqsen": "\\ClaseX",
+            "name": "ClaseX",
+            "summary": "Clase\u0020X\u0020con\u0020la\u0020crear\u0020los\u0020datos\u0020que\u0020deseamos\u0020meter\u0020en\u0020la\u0020BD",
+            "url": "classes/ClaseX.html"
+        },                {
+            "fqsen": "\\ClaseX\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor\u0020para\u0020inicializar\u0020los\u0020objetos",
+            "url": "classes/ClaseX.html#method___construct"
+        },                {
+            "fqsen": "\\ClaseX\u003A\u003AALTA_BD\u0028\u0029",
+            "name": "ALTA_BD",
+            "summary": "M\u00E9todo\u0020con\u0020el\u0020que\u0020creamos\u0020la\u0020conexi\u00F3n\u0020con\u0020la\u0020BD\u0020e\u0020introducimos\u0020los\u0020datos\u0020especificados\u0020en\u0020el\u0020constructor",
+            "url": "classes/ClaseX.html#method_ALTA_BD"
+        },                {
+            "fqsen": "\\ClaseX\u003A\u003AVER_T\u0028\u0029",
+            "name": "VER_T",
+            "summary": "M\u00E9todo\u0020con\u0020el\u0020que\u0020visualizamos\u0020todos\u0020los\u0020datos\u0020dentro\u0020de\u0020la\u0020tabla\u0020creada\u0020en\u0020la\u0020BD",
+            "url": "classes/ClaseX.html#method_VER_T"
+        },                {
+            "fqsen": "\\ClaseX\u003A\u003A\u0024c1",
+            "name": "c1",
+            "summary": "",
+            "url": "classes/ClaseX.html#property_c1"
+        },                {
+            "fqsen": "\\ClaseX\u003A\u003A\u0024c2",
+            "name": "c2",
+            "summary": "",
+            "url": "classes/ClaseX.html#property_c2"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
